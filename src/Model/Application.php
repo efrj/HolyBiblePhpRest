8f28dbd26411b00e0a01cdd7fcee60bd0034180f
efrj <efrj@hotmail.com>
2016-03-28 23:57:07 -0300

    Initial Commit

---
+++ src/Model/Application.php
@@ -0,0 +1,11 @@
+<?php
+namespace BibleRest\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bible extends Model {
+
+    protected $table = "applications";
+    public $timestamps = false;
+}
+?>
---
+++ src/Model/Bible.php
@@ -0,0 +1,9 @@
+<?php
+namespace BibleRest\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bible extends Model {
+    protected $table = "Bible";
+}
+?>
