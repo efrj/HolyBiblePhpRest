4ae78c1b1869d0a3af34f9fb38b61624df29ce03
efrj <efrj@hotmail.com>
2016-03-30 05:21:40 -0300

    Add route to get Bible Verse. Add sqlite file database.

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,15 +1,11 @@
 <?php
-// DIC configuration
-
 $container = $app->getContainer();
 
-// view renderer
 $container['renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
     return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
-// monolog
 $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
--- src/routes.php
+++ src/routes.php
@@ -3,3 +3,18 @@
 $app->get('/', function ($request, $response) {
     return $this->renderer->render($response, 'index.phtml');
 });
+
+$app->get('/book/{book}/chapter/{chapter}/verse/{verse}', function($request, $response) {
+    
+    $book = $request->getAttribute('book');
+    $chapter = $request->getAttribute('chapter');
+    $verse = $request->getAttribute('verse');
+
+    $data = \BibleRest\Model\Bible::select('BookName AS book','Chapter as chapter', 'Verse as verse', 'VText as text')
+                ->where('BookAbr', $book)
+                ->where('Chapter', $chapter)
+                ->where('Verse', $verse)
+                ->get();
+    
+    echo $data['0'];
+});
