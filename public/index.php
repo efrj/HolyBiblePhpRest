06eccf4aef490c7f70d0acbc32a0080ddceb2ff6
efrj <efrj@hotmail.com>
2016-03-30 05:21:18 -0300

    Add route to get Bible Verse. Add sqlite file database.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'   => 'sqlite',
+    'database' => __DIR__.'/../db/KJV-PCE.db',
+    'prefix'   => '',
+], 'default');
+$capsule->bootEloquent();
+$capsule->setAsGlobal();
 
 session_start();
 
